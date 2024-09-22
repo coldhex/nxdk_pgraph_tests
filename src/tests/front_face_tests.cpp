f9e9caf1c8d5418ea646e966d170794ee954551b
coldhex <coldhex@localhost>
2024-09-22 19:45:22 +0300

    Add front face tests for polygon line mode
    Also adds zero projected-area triangles. In polygon line mode, Xbox renders
    such triangles as lines under some conditions.

--- src/tests/front_face_tests.cpp
+++ src/tests/front_face_tests.cpp
@@ -24,12 +24,14 @@ static constexpr uint32_t kCullFaces[] = {
 
 FrontFaceTests::FrontFaceTests(TestHost& host, std::string output_dir)
     : TestSuite(host, std::move(output_dir), "Front face") {
-  for (auto winding : kWindings) {
-    for (auto cull_face : kCullFaces) {
-      std::string name = MakeTestName(winding, cull_face);
+  for (auto line_mode : {false, true}) {
+    for (auto winding : kWindings) {
+      for (auto cull_face : kCullFaces) {
+	std::string name = MakeTestName(winding, cull_face, line_mode);
 
-      auto test = [this, winding, cull_face]() { this->Test(winding, cull_face); };
-      tests_[name] = test;
+	auto test = [this, winding, cull_face, line_mode]() { this->Test(winding, cull_face, line_mode); };
+	tests_[name] = test;
+      }
     }
   }
 }
@@ -60,7 +62,8 @@ void FrontFaceTests::CreateGeometry() {
   float mid_width = left + (right - left) * 0.5f;
 
   uint32_t num_quads = 2;
-  std::shared_ptr<VertexBuffer> buffer = host_.AllocateVertexBuffer(6 * num_quads);
+  uint32_t num_tris = 2;
+  std::shared_ptr<VertexBuffer> buffer = host_.AllocateVertexBuffer(6 * num_quads + 3 * num_tris);
   buffer->SetPositionIncludesW();
 
   Color ul{1.0, 0.0, 0.0, 1.0};
@@ -70,6 +73,11 @@ void FrontFaceTests::CreateGeometry() {
 
   float z = 10.0f;
   buffer->DefineBiTriCCW(0, left + 10, top + 4, mid_width - 10, bottom - 10, z, z, z, z, ul, ll, lr, ur);
+  float tri_one[3] = {left + 5, top + 4, z};
+  float tri_two[3] = {left + 5, bottom - 10, z};
+  float tri_three[3] = {left + 5, (top + bottom - 6) / 2, z};
+  float normal[3] = {0.0f, 0.0f, 1.0f};
+  buffer->DefineTriangleCCW(4, tri_one, tri_two, tri_three, normal, normal, normal, ul, ll, lr);
 
   buffer->SetPositionIncludesW();
   auto vtx = buffer->Lock();
@@ -79,6 +87,12 @@ void FrontFaceTests::CreateGeometry() {
   vtx[3].pos[3] = INFINITY;
   vtx[4].pos[3] = INFINITY;
   vtx[5].pos[3] = INFINITY;
+  vtx[12].pos[3] = 1.0f;
+  vtx[13].pos[3] = 1.0f;
+  vtx[14].pos[3] = 1.0f;
+  vtx[15].pos[3] = 1.0f;
+  vtx[16].pos[3] = 1.0f;
+  vtx[17].pos[3] = 1.0f;
   buffer->Unlock();
 
   Color tmp = ul;
@@ -90,9 +104,13 @@ void FrontFaceTests::CreateGeometry() {
   ll = tmp;
 
   buffer->DefineBiTri(1, mid_width + 10, top + 4, right - 10, bottom - 10, z, z, z, z, ul, ll, lr, ur);
+  float tri2_one[3] = {right - 5, top + 4, z};
+  float tri2_two[3] = {right - 5, bottom - 10, z};
+  float tri2_three[3] = {right - 5, (top + bottom - 6) / 2, z};
+  buffer->DefineTriangle(5, tri2_one, tri2_two, tri2_three, normal, normal, normal, ul, ll, lr);
 }
 
-void FrontFaceTests::Test(uint32_t front_face, uint32_t cull_face) {
+void FrontFaceTests::Test(uint32_t front_face, uint32_t cull_face, bool line_mode) {
   host_.PrepareDraw();
 
   // To verify that the HW is simply preserving a previously set value, force it to a known valid, but different value
@@ -115,9 +133,21 @@ void FrontFaceTests::Test(uint32_t front_face, uint32_t cull_face) {
   auto p = pb_begin();
   p = pb_push1(p, NV097_SET_FRONT_FACE, front_face);
   p = pb_push1(p, NV097_SET_CULL_FACE, cull_face);
+  if (line_mode) {
+    p = pb_push1(p, NV097_SET_FRONT_POLYGON_MODE, NV097_SET_FRONT_POLYGON_MODE_V_LINE);
+    p = pb_push1(p, NV097_SET_BACK_POLYGON_MODE, NV097_SET_FRONT_POLYGON_MODE_V_LINE);
+  }
   pb_end(p);
   host_.DrawArrays();
 
+  if (line_mode) {
+    p = pb_begin();
+    p = pb_push1(p, NV097_SET_FRONT_POLYGON_MODE, NV097_SET_FRONT_POLYGON_MODE_V_FILL);
+    p = pb_push1(p, NV097_SET_BACK_POLYGON_MODE, NV097_SET_FRONT_POLYGON_MODE_V_FILL);
+    pb_end(p);
+  }
+
+  pb_print(line_mode ? "Line mode\n" : "Fill mode\n");
   std::string winding_name = WindingName(front_face);
   pb_print("FF: %s\n", winding_name.c_str());
   std::string cull_face_name = CullFaceName(cull_face);
@@ -126,16 +156,17 @@ void FrontFaceTests::Test(uint32_t front_face, uint32_t cull_face) {
   pb_printat(8, 38, (char*)"CW");
   pb_draw_text_screen();
 
-  std::string name = MakeTestName(front_face, cull_face);
+  std::string name = MakeTestName(front_face, cull_face, line_mode);
   host_.FinishDraw(allow_saving_, output_dir_, name);
 }
 
-std::string FrontFaceTests::MakeTestName(uint32_t front_face, uint32_t cull_face) {
+std::string FrontFaceTests::MakeTestName(uint32_t front_face, uint32_t cull_face, bool line_mode) {
   std::string ret = "FrontFace_";
+  ret += line_mode ? "LM_" : "FM_";
   ret += WindingName(front_face);
   ret += "_CF_";
   ret += CullFaceName(cull_face);
-  return std::move(ret);
+  return ret;
 }
 
 static std::string WindingName(uint32_t winding) {
